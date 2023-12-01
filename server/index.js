1477532df8703eccac09d310bcee9923f1e9fd09
Elissa <elissadusabe@gmail.com>
2023-12-01 21:49:17 +0200

    new changes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import multer from 'multer';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
+import json from 'jsonwebtoken'
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
