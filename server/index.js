0fb47bd3e616d50cb63aaa40d8b0a209ad059104
Elissa <elissadusabe@gmail.com>
2023-12-02 20:23:46 +0200

    mongo setup

--- server/index.js
+++ server/index.js
@@ -17,11 +17,29 @@ const app = express();
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.subscribe("/assets", express.static(path.join(__dirname, 'public/assets')));
 
+// FILE STORAGE
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
 
+const upload = multer({ storage });
 
-
-
-
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 5005;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    UseUnififedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`💪Server running at Port: ${PORT}`))
+})
+.catch((error) => console.log(`Error: ${error}  did not connect😒`))
