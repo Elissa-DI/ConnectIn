0f1e9fdab529cd15410e8feeace6edde5d2b404b
Elissa <elissadusabe@gmail.com>
2023-12-03 09:38:44 +0200

    new changes in auth

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -44,8 +44,20 @@ export const addRemoveFriend = async (req, res) => {
             user.friends.push(friendId);
             friend.friends.push(id);
         }
+
         await user.save();
         await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return { _id, firstName, lastName, occupation, location, picturePath };
+            }
+        );
+
+        res.status(200).json(formattedFriends);
     } catch (error) {
         res.status(404).json({ message: error.message });        
     }
