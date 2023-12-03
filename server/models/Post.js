d54bab8f08dd148714ae30b8913a3202427a5f19
Elissa <elissadusabe@gmail.com>
2023-12-03 10:02:46 +0200

    new changes in auth

---
+++ server/models/Post.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
