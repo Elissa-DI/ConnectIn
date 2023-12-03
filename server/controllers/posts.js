b8dbbe0668341d15a8e45fb04eee3052825d1c23
Elissa <elissadusabe@gmail.com>
2023-12-03 10:09:41 +0200

    new changes in auth

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -17,7 +17,30 @@ export const createPost = async (req, res) => {
             comments: [],
         });
         await newPost.save();
-    } catch (error) {
 
+        const post = await Post.find();
+        res.status(201).json(post);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
     }
 }
+
+/* READ */
+export const getFeedPosts = async (req, res) => {
+    try {
+      const post = await Post.find();
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  };
+  
+  export const getUserPosts = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const post = await Post.find({ userId });
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  };
