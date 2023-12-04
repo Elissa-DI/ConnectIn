0468c66d289947c433f70b92a8fa4bda53430b39
Elissa <elissadusabe@gmail.com>
2023-12-04 10:32:34 +0200

    data added

--- server/data/index.js
+++ server/data/index.js
@@ -164,3 +164,42 @@ export const posts = [
       "yet another random comment",
     ],
   },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[3],
+    firstName: "Whatcha",
+    lastName: "Doing",
+    location: "Korea, CA",
+    description:
+      "Another really long random description. This one is longer than the previous one.",
+    picturePath: "post2.jpeg",
+    userPicturePath: "p6.jpeg",
+    likes: new Map([
+      [userIds[7], true],
+      [userIds[4], true],
+      [userIds[1], true],
+      [userIds[2], true],
+    ]),
+    comments: [
+      "one more random comment",
+      "and another random comment",
+      "no more random comments",
+      "I lied, one more random comment",
+    ],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[4],
+    firstName: "Jane",
+    lastName: "Doe",
+    location: "Utah, CA",
+    description:
+      "This is the last really long random description. This one is longer than the previous one.",
+    picturePath: "post3.jpeg",
+    userPicturePath: "p5.jpeg",
+    likes: new Map([
+      [userIds[1], true],
+      [userIds[6], true],
+      [userIds[3], true],
+      [userIds[5], true],
+    ]),
