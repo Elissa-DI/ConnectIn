53cff042c4fc7d69f957f9901a012061974c5f79
Elissa <elissadusabe@gmail.com>
2023-12-04 10:32:17 +0200

    data added

--- server/data/index.js
+++ server/data/index.js
@@ -124,3 +124,43 @@ export const users = [
     updatedAt: 1642716557,
     __v: 0,
   },
+  {
+    _id: userIds[7],
+    firstName: "Jessica",
+    lastName: "Dunn",
+    email: "jessicadunn@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p9.jpeg",
+    friends: [],
+    location: "Washington, DC",
+    occupation: "A Student",
+    viewedProfile: 19420,
+    impressions: 82970,
+    createdAt: 1369908044,
+    updatedAt: 1359322268,
+    __v: 0,
+  },
+];
+
+export const posts = [
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[1],
+    firstName: "Steve",
+    lastName: "Ralph",
+    location: "New York, CA",
+    description: "Some really long random description",
+    picturePath: "post1.jpeg",
+    userPicturePath: "p3.jpeg",
+    likes: new Map([
+      [userIds[0], true],
+      [userIds[2], true],
+      [userIds[3], true],
+      [userIds[4], true],
+    ]),
+    comments: [
+      "random comment",
+      "another random comment",
+      "yet another random comment",
+    ],
+  },
